dc7783b4250e79001029a11e9d4d851c42aee60b
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-10-13 20:47:37 +0530

    revision of a  components

--- App.js
+++ App.js
@@ -1,35 +1,11 @@
-/**
- * // React Element = > Object > render > Htmlelement
-
-const heading = React.createElement("h1", { id: "heading" }, "This is h1 tag ");
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
- *
- *   <div id = "Parent">
- *      <div id = "Child">
- *       <h1> i am h1 tag </h1>
- *       <h2> i am h2 tag </h2>
- *      <div>
- *   <div>
- *  // const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hello World From React"
-// );
- */
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const TitleComponent = () => (
-  <h1 id="heading ">Nihal is the greart developer is there </h1>
-);
+const title = <h1 id="heading ">Nihal is the greart developer is there </h1>;
 
 const HeadingComponent = () => (
   <div id="container">
-    <TitleComponent />
+    {title}
     <h1> this is functional component </h1>;
   </div>
 );
