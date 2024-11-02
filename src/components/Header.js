f9722632cb56298eead7f637912498e88abc70ec
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-11-02 16:22:20 +0530

    useState hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+
 const Header = () => {
+  const [btnName, setbtnName] = useState("login");
+  console.log(" Header render");
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +16,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnName === "login" ? setbtnName("logOut") : setbtnName("login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>{" "}
       </div>
     </div>
