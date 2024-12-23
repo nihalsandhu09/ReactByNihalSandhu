c5070d8ba087d4e38b6115821043b45f4e294c59
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-12-23 13:55:53 +0530

    testing header component

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Header from "../Header";
 import { Provider } from "react-redux";
 import appStore from "../../utils/appStore";
@@ -18,4 +18,33 @@ it("should render Header Component with a Cart items 0", () => {
   //   const LogInButton = screen.getByText("Log In");
 
   expect(cartItems).toBeInTheDocument();
+});
+it("should render Header Component with a Cart item", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const cartItems = screen.getByText(/Cart/); // good way
+  //   const LogInButton = screen.getByText("Log In");
+
+  expect(cartItems).toBeInTheDocument();
+});
+
+it("should render Header Component with a login component", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const logIn = screen.getByRole("button"); // good way
+  //   const LogInButton = screen.getByText("Log In");
+
+  expect(logIn).toBeInTheDocument();
 });
